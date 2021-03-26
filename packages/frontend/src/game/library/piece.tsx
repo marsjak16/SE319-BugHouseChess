b43f92547575a0997094bde805eeb0d6f313a467
CJ Olsen <cjols97@iastate.edu>
2021-03-26 03:25:07 -0500

    Chess piece class

---
+++ packages/frontend/src/game/library/piece.tsx
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+
+export class Piece extends Component {
+
+
+    render () {
+        return (
+            <div>
+            {/* TODO Render each piece */}
+            {/*  <kingPiece/>*/}
+            {/*  <queenPiece/>*/}
+            </div>
+        );
+    }
+}
+
+const kingPiece = class KingPiece extends Component {
+    render () {
+        return (
+            <div>
+            {/* TODO */}
+            </div>
+        )
+    }
+}
+
