997537c7f76d52b7f7e4484112af0ea9d9d103ee
Marcus <marsjak16@gmail.com>
2021-04-19 22:58:51 -0500

    movement file created and movement model

---
+++ packages/public/game/movement.ts
@@ -0,0 +1,6 @@
+import {PieceMoveRequest} from "../models/game/piece-move-request";
+import {DescribePossibleMovement} from "../models/game/describe-possible-movement";
+
+export function movement(moveRequest: PieceMoveRequest): DescribePossibleMovement[] {
+    return [];
+}
---
+++ packages/public/models/game/describe-possible-movement.ts
@@ -0,0 +1,5 @@
+export interface DescribePossibleMovement {
+    boardNum: 1 | 2,
+    xCord: number,
+    yCord: number
+}
