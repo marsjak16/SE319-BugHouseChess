33703ba139c2c9542a2a6afbcc6ddf29017f4422
Marcus <marsjak16@gmail.com>
2021-04-19 23:02:45 -0500

    updated function

--- packages/public/game/movement.ts
+++ packages/public/game/movement.ts
@@ -1,6 +1,7 @@
 import {PieceMoveRequest} from "../models/game/piece-move-request";
 import {DescribePossibleMovement} from "../models/game/describe-possible-movement";
+import {Game} from "../models/game/Game";
 
-export function movement(moveRequest: PieceMoveRequest): DescribePossibleMovement[] {
+export function movement(moveRequest: PieceMoveRequest, game: Game): DescribePossibleMovement[] {
     return [];
 }
